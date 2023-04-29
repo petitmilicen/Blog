af18dad9048fce9c1c20e96c818925559c5bebfa
deintrix <danilo.luque18@gmail.com>
2023-04-29 15:13:05 -0400

    agrego id post

--- js/publicacion-validacion.js
+++ js/publicacion-validacion.js
@@ -7,7 +7,7 @@ $().ready(function() {
                 minlength: 20
             },
            
-            comentario: {
+            texto: {
                 required: true,
                 minlength: 200
             }
@@ -17,7 +17,7 @@ $().ready(function() {
                 required: "Debes completar tu titulo",
                 minlength: "Tu titulo debe de tener al menos 20 caracteres"
             },
-            comentario: {
+            texto: {
                 required: "El campo de texto es obligatorio",
                 minlength: "El campo de texto debe tener al menos 200 caracteres"
             }
