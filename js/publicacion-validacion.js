66221bb1dfd9e0b7f67e0767011864e098a9d0b9
petitmilicen <127413966+petitmilicen@users.noreply.github.com>
2023-05-02 11:40:03 -0400

    Detalles validacion

--- js/publicacion-validacion.js
+++ js/publicacion-validacion.js
@@ -4,23 +4,27 @@ $().ready(function() {
         rules: {
             titulo: {
                 required: true,
-                minlength: 20
+                minlength: 20,
+                maxlength: 40
             },
            
             texto: {
                 required: true,
                 minlength: 200
-            }
+            },
+
         },
         messages: {
             titulo: {
                 required: "Debes completar tu titulo",
-                minlength: "Tu titulo debe de tener al menos 20 caracteres"
+                minlength: "Tu titulo debe de tener al menos 20 caracteres",
+                maxlength: "El maximo del titulo es de un maximo de 40 caracteres"
             },
             texto: {
                 required: "El campo de texto es obligatorio",
                 minlength: "El campo de texto debe tener al menos 200 caracteres"
-            }
+            },
+
     
         }
 
